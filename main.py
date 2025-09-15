7112f3a66a86c92f078fc3fc642c6fd6096ecfaa
Anish Pechetti <work.anish.pechetti@gmail.com>
2025-09-15 20:59:32 +0530

    Another test

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from jugaad_data.nse import NSELive
 client = NSELive()
 data = client.live_fno()
 print(data.keys())
-print(data.get("data", [])[:3])  # sample first 5 entries
+print(data.get("data", [])[:5])  # sample first 5 entries
